dde31773db82cc68529cf4057077dde85808aec4
eleuterio3d <eleuterio@gestaointeligentesoftware.com.br>
2021-11-03 23:24:40 +0200

    first commit

---
+++ src/app/cart.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Product } from './products';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CartService {
+  items: Product[] = [];
+
+  constructor(private http: HttpClient) {}
+
+
+
+  getItems() {
+    return this.items;
+  }
+
+
+}
---
+++ src/app/products.ts
@@ -0,0 +1,28 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Phone XL',
+    price: 700000,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    id: 2,
+    name: 'Phone XR',
+    price: 167000,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    id: 3,
+    name: 'GALAX 20',
+    price: 299000,
+    description: 'Novo'
+  }
+];
+
